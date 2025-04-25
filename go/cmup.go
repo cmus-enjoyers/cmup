58d6d59ef6ce348db7fc2886ac9bd75075e1da16
VKTRenokh <viktorenokh@gmail.com>
2025-04-25 16:26:49 +0300

    feat: more rofls from tour of go

--- go/cmup.go
+++ go/cmup.go
@@ -11,4 +11,10 @@ var z complex128 = cmplx.Sqrt(-5 + 12i)
 func main() {
 	fmt.Println(math.Abs(-1))
 	fmt.Println(z, "goida")
+
+	bestNumberInTheWorld := 14
+
+	bestNumberInTheWorldFloat := float32(bestNumberInTheWorld) + 0.1
+
+	fmt.Println(bestNumberInTheWorldFloat, "afdsadsfsdfasdfsdassdfsdfsdfsf ")
 }

79aabcd200567efc557fe9b0ad16d678c0188afa
VKTRenokh <viktorenokh@gmail.com>
2025-04-28 14:58:41 +0300

    feat: structs???

--- go/cmup.go
+++ go/cmup.go
@@ -2,43 +2,15 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"math/cmplx"
-	"math/rand"
 )
 
-var x complex128 = cmplx.Sqrt(-5 + 12i)
-
-func Sqrt(x float64) float64 {
-	z := 1.0
-
-	for i := 0; i < 10; i++ {
-		z -= (z*z - x) / (2 * z)
-	}
-
-	return z
+type Playlist struct {
+	name         string
+	subPlaylists []*Playlist
 }
 
 func main() {
-	fmt.Println(math.Abs(-1))
+	var playlist = Playlist{name: "Test Playlist"}
 
-	const someThing = "my!lane"
-
-	bestNumberInTheWorld := 14
-
-	bestNumberInTheWorldFloat := float32(bestNumberInTheWorld) + 0.1
-
-	fmt.Printf("xx %v %T\n ", bestNumberInTheWorldFloat, bestNumberInTheWorldFloat)
-
-	fmt.Println(someThing, "constant", x)
-
-	for i := 1000; i > 0; i -= 7 {
-		fmt.Println("x", i-7)
-	}
-
-	if x := rand.Float32(); x > 0.5 {
-		fmt.Println("random num is greater than 0.5", x)
-	}
-
-	fmt.Println(Sqrt(52))
+	fmt.Println(playlist)
 }

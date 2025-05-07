9b716adbfdb639b7ccaeb55d37770e90e5c183fe
VKTRenokh <viktorenokh@gmail.com>
2025-05-07 13:14:22 +0300

    feat: improve dir reading

--- go/cmup.go
+++ go/cmup.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path"
-	"strings"
+	// "strings"
 )
 
 type Playlist struct {
@@ -16,78 +16,20 @@ func (playlist Playlist) Print() {
 	fmt.Println("Playlist", playlist.name, "len", len(playlist.subPlaylists))
 }
 
-func Pic(dx, dy int) [][]uint8 {
-	var arry = make([][]uint8, dy)
+func cmup(homePath string) {
+	dir, err := os.ReadDir(path.Join(homePath, "Music"))
 
-	for y := range arry {
-		arry[y] = make([]uint8, dx)
-
-		for x := range arry[y] {
-			arry[y][x] = uint8((x + y) / 2)
-		}
-	}
-
-	return arry
-}
-
-func countWords(s string) map[string]int {
-	fields := strings.Fields(s)
-
-	result := map[string]int{}
-
-	for _, value := range fields {
-		result[value] += 1
-	}
-
-	return result
-}
-
-func log(fn func(x, y uint8) uint8) func(x, y uint8) uint8 {
-	fmt.Println()
-
-	return func(x, y uint8) uint8 {
-		result := fn(x, y)
-
-		fmt.Println("[LOG] result: ", result)
-
-		return result
+	if err == nil {
+		fmt.Println(dir)
 	}
 }
 
 func main() {
-	var playlist = Playlist{name: "Test Playlist"}
+	home, err := os.UserHomeDir()
 
-	playlist.Print()
-
-	var playlists = make([]Playlist, 0)
-
-	var home, err = os.UserHomeDir()
-
-	if err != nil {
-		fmt.Println("Couldn't determine user home dir.")
-		return
+	if err == nil {
+		cmup(home)
+	} else {
+		fmt.Println(err, "error")
 	}
-
-	fmt.Println(playlist)
-
-	fmt.Println(os.ReadDir(path.Join(home, "Music")))
-
-	playlists = append(playlists, playlist)
-
-	coolMap := make(map[string]int)
-
-	coolMap["zxc"] = 1000 - 7
-
-	coolMap["three"] = 3
-	coolMap["two"] = 2
-
-	delete(coolMap, "two")
-
-	fmt.Println(countWords("I am learning Go! Go!"))
-
-	test := log(func(x, y uint8) uint8 {
-		return x ^ y
-	})
-
-	test(10, 15)
 }

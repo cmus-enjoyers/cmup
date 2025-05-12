77244c6b507fc073757d351913617ca0adc73d05
VKTRenokh <viktorenokh@gmail.com>
2025-05-12 16:26:06 +0300

    feat: implement dir reading & improve error handling

--- go/cmup.go
+++ go/cmup.go
@@ -18,21 +18,27 @@ func (playlist Playlist) Print() {
 	fmt.Println("Playlist", playlist.name, "len", len(playlist.subPlaylists))
 }
 
-func readPlaylist(dir os.DirEntry, path string) (Playlist, error) {
+func readPlaylist(dir os.DirEntry, dirPath string) (Playlist, error) {
 	if !dir.IsDir() {
 		return Playlist{dir.Name(), make([]string, 0), make([]*Playlist, 0)}, errors.New("Dir isn't a dir")
 	}
 
-	content, err := os.ReadDir(path)
+	content, err := os.ReadDir(dirPath)
 
 	if err == nil {
-		return Playlist{dir.Name(), make([]string, 0), make([]*Playlist, 0)}, nil
+		result := make([]string, 0)
+
+		for _, value := range content {
+			result = append(result, path.Join(dirPath, value.Name()))
+		}
+
+		return Playlist{dir.Name(), result, make([]*Playlist, 0)}, nil
 	}
 
 	return Playlist{dir.Name(), make([]string, 0), make([]*Playlist, 0)}, err
 }
 
-func cmup(homePath string) []Playlist {
+func cmup(homePath string) ([]Playlist, error) {
 	dir, err := os.ReadDir(path.Join(homePath, "Music"))
 
 	result := make([]Playlist, 0)
@@ -47,16 +53,24 @@ func cmup(homePath string) []Playlist {
 				}
 			}
 		}
+
+		return result, nil
 	}
 
-	return result
+	return make([]Playlist, 0), err
 }
 
 func main() {
 	home, err := os.UserHomeDir()
 
 	if err == nil {
-		fmt.Println(cmup(home))
+		result, err := cmup(home)
+
+		if err == nil {
+			fmt.Println(result)
+		} else {
+			fmt.Println("Gmup errored :(", err)
+		}
 	} else {
 		fmt.Println(err, "error")
 	}
